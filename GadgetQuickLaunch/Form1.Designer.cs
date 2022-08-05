85f3035cc7697ba3bc93965929101bb60c95deb9
ROSGP <rosgameplay71@gmail.com>
2022-08-06 00:38:13 +0300

    Добавьте файлы проекта.

---
+++ GadgetQuickLaunch/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace GadgetQuickLaunch
+	{
+	partial class Form1
+		{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+			{
+			if (disposing && (components != null))
+				{
+				components.Dispose();
+				}
+			base.Dispose(disposing);
+			}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+			{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.btCloseForm = new System.Windows.Forms.Button();
+			this.btAwesomeMiner = new System.Windows.Forms.Button();
+			this.btWOT = new System.Windows.Forms.Button();
+			this.bt_MS_Visual_Studio = new System.Windows.Forms.Button();
+			this.bt_MS_Visual_Studio_Code = new System.Windows.Forms.Button();
+			this.bt_NotePadPlusPlus = new System.Windows.Forms.Button();
+			this.label_btAwesomeMiner = new System.Windows.Forms.Label();
+			this.label_bt_MS_Visual_Studio = new System.Windows.Forms.Label();
+			this.label_bt_MS_VS_Code = new System.Windows.Forms.Label();
+			this.label_bt_NotePadPlusPlus = new System.Windows.Forms.Label();
+			this.label_btWOT = new System.Windows.Forms.Label();
+			this.btnFocus = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btCloseForm
+			// 
+			this.btCloseForm.BackColor = System.Drawing.Color.IndianRed;
+			this.btCloseForm.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+			this.btCloseForm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btCloseForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.btCloseForm.ForeColor = System.Drawing.Color.Black;
+			this.btCloseForm.Location = new System.Drawing.Point(2, 2);
+			this.btCloseForm.Name = "btCloseForm";
+			this.btCloseForm.Size = new System.Drawing.Size(180, 19);
+			this.btCloseForm.TabIndex = 1;
+			this.btCloseForm.Text = "З А К Р Ы Т Ь";
+			this.btCloseForm.UseVisualStyleBackColor = false;
+			this.btCloseForm.Click += new System.EventHandler(this.btCloseForm_Click);
+			// 
+			// btAwesomeMiner
+			// 
+			this.btAwesomeMiner.BackColor = System.Drawing.Color.Transparent;
+			this.btAwesomeMiner.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.btAwesomeMiner.FlatAppearance.BorderSize = 0;
+			this.btAwesomeMiner.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+			this.btAwesomeMiner.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.btAwesomeMiner.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btAwesomeMiner.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.btAwesomeMiner.ForeColor = System.Drawing.Color.Black;
+			this.btAwesomeMiner.Image = ((System.Drawing.Image)(resources.GetObject("btAwesomeMiner.Image")));
+			this.btAwesomeMiner.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.btAwesomeMiner.Location = new System.Drawing.Point(3, 28);
+			this.btAwesomeMiner.Name = "btAwesomeMiner";
+			this.btAwesomeMiner.Size = new System.Drawing.Size(180, 35);
+			this.btAwesomeMiner.TabIndex = 2;
+			this.btAwesomeMiner.Text = "Awesome Miner";
+			this.btAwesomeMiner.UseVisualStyleBackColor = false;
+			this.btAwesomeMiner.Click += new System.EventHandler(this.btAwesomeMiner_Click);
+			// 
+			// btWOT
+			// 
+			this.btWOT.BackColor = System.Drawing.Color.Transparent;
+			this.btWOT.FlatAppearance.BorderSize = 0;
+			this.btWOT.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+			this.btWOT.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.btWOT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btWOT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+			this.btWOT.ForeColor = System.Drawing.Color.Black;
+			this.btWOT.Image = ((System.Drawing.Image)(resources.GetObject("btWOT.Image")));
+			this.btWOT.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.btWOT.Location = new System.Drawing.Point(2, 196);
+			this.btWOT.Name = "btWOT";
+			this.btWOT.Size = new System.Drawing.Size(180, 35);
+			this.btWOT.TabIndex = 3;
+			this.btWOT.Text = "World Of Tanks";
+			this.btWOT.UseVisualStyleBackColor = false;
+			this.btWOT.Click += new System.EventHandler(this.btWOT_Click);
+			// 
+			// bt_MS_Visual_Studio
+			// 
+			this.bt_MS_Visual_Studio.BackColor = System.Drawing.Color.Transparent;
+			this.bt_MS_Visual_Studio.FlatAppearance.BorderSize = 0;
+			this.bt_MS_Visual_Studio.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+			this.bt_MS_Visual_Studio.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.bt_MS_Visual_Studio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.bt_MS_Visual_Studio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+			this.bt_MS_Visual_Studio.ForeColor = System.Drawing.Color.Black;
+			this.bt_MS_Visual_Studio.Image = ((System.Drawing.Image)(resources.GetObject("bt_MS_Visual_Studio.Image")));
+			this.bt_MS_Visual_Studio.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.bt_MS_Visual_Studio.Location = new System.Drawing.Point(2, 70);
+			this.bt_MS_Visual_Studio.Name = "bt_MS_Visual_Studio";
+			this.bt_MS_Visual_Studio.Size = new System.Drawing.Size(180, 35);
+			this.bt_MS_Visual_Studio.TabIndex = 4;
+			this.bt_MS_Visual_Studio.Text = "MS Visual Studio    ";
+			this.bt_MS_Visual_Studio.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.bt_MS_Visual_Studio.UseVisualStyleBackColor = false;
+			this.bt_MS_Visual_Studio.Click += new System.EventHandler(this.bt_MS_Visual_Studio_Click);
+			// 
+			// bt_MS_Visual_Studio_Code
+			// 
+			this.bt_MS_Visual_Studio_Code.BackColor = System.Drawing.Color.Transparent;
+			this.bt_MS_Visual_Studio_Code.FlatAppearance.BorderSize = 0;
+			this.bt_MS_Visual_Studio_Code.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+			this.bt_MS_Visual_Studio_Code.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.bt_MS_Visual_Studio_Code.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.bt_MS_Visual_Studio_Code.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+			this.bt_MS_Visual_Studio_Code.ForeColor = System.Drawing.Color.Black;
+			this.bt_MS_Visual_Studio_Code.Image = ((System.Drawing.Image)(resources.GetObject("bt_MS_Visual_Studio_Code.Image")));
+			this.bt_MS_Visual_Studio_Code.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.bt_MS_Visual_Studio_Code.Location = new System.Drawing.Point(2, 112);
+			this.bt_MS_Visual_Studio_Code.Name = "bt_MS_Visual_Studio_Code";
+			this.bt_MS_Visual_Studio_Code.Size = new System.Drawing.Size(180, 35);
+			this.bt_MS_Visual_Studio_Code.TabIndex = 5;
+			this.bt_MS_Visual_Studio_Code.Text = "MS VS Code";
+			this.bt_MS_Visual_Studio_Code.UseVisualStyleBackColor = false;
+			this.bt_MS_Visual_Studio_Code.Click += new System.EventHandler(this.bt_MS_Visual_Studio_Code_Click);
+			// 
+			// bt_NotePadPlusPlus
+			// 
+			this.bt_NotePadPlusPlus.BackColor = System.Drawing.Color.Transparent;
+			this.bt_NotePadPlusPlus.FlatAppearance.BorderSize = 0;
+			this.bt_NotePadPlusPlus.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+			this.bt_NotePadPlusPlus.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+			this.bt_NotePadPlusPlus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.bt_NotePadPlusPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+			this.bt_NotePadPlusPlus.ForeColor = System.Drawing.Color.Black;
+			this.bt_NotePadPlusPlus.Image = ((System.Drawing.Image)(resources.GetObject("bt_NotePadPlusPlus.Image")));
+			this.bt_NotePadPlusPlus.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.bt_NotePadPlusPlus.Location = new System.Drawing.Point(2, 154);
+			this.bt_NotePadPlusPlus.Name = "bt_NotePadPlusPlus";
+			this.bt_NotePadPlusPlus.Size = new System.Drawing.Size(180, 35);
+			this.bt_NotePadPlusPlus.TabIndex = 6;
+			this.bt_NotePadPlusPlus.Text = "NotePad++";
+			this.bt_NotePadPlusPlus.UseVisualStyleBackColor = false;
+			this.bt_NotePadPlusPlus.Click += new System.EventHandler(this.bt_NotePadPlusPlus_Click);
+			// 
+			// label_btAwesomeMiner
+			// 
+			this.label_btAwesomeMiner.AutoSize = true;
+			this.label_btAwesomeMiner.BackColor = System.Drawing.Color.Transparent;
+			this.label_btAwesomeMiner.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.label_btAwesomeMiner.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+			this.label_btAwesomeMiner.ForeColor = System.Drawing.Color.White;
+			this.label_btAwesomeMiner.Location = new System.Drawing.Point(32, 35);
+			this.label_btAwesomeMiner.Name = "label_btAwesomeMiner";
+			this.label_btAwesomeMiner.Size = new System.Drawing.Size(116, 16);
+			this.label_btAwesomeMiner.TabIndex = 7;
+			this.label_btAwesomeMiner.Text = "Awesome Miner";
+			// 
+			// label_bt_MS_Visual_Studio
+			// 
+			this.label_bt_MS_Visual_Studio.AutoSize = true;
+			this.label_bt_MS_Visual_Studio.BackColor = System.Drawing.Color.Transparent;
+			this.label_bt_MS_Visual_Studio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+			this.label_bt_MS_Visual_Studio.ForeColor = System.Drawing.Color.White;
+			this.label_bt_MS_Visual_Studio.Location = new System.Drawing.Point(36, 77);
+			this.label_bt_MS_Visual_Studio.Name = "label_bt_MS_Visual_Studio";
+			this.label_bt_MS_Visual_Studio.Size = new System.Drawing.Size(124, 16);
+			this.label_bt_MS_Visual_Studio.TabIndex = 8;
+			this.label_bt_MS_Visual_Studio.Text = "MS Visual Studio";
+			// 
+			// label_bt_MS_VS_Code
+			// 
+			this.label_bt_MS_VS_Code.AutoSize = true;
+			this.label_bt_MS_VS_Code.BackColor = System.Drawing.Color.Transparent;
+			this.label_bt_MS_VS_Code.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+			this.label_bt_MS_VS_Code.ForeColor = System.Drawing.Color.White;
+			this.label_bt_MS_VS_Code.Location = new System.Drawing.Point(43, 119);
+			this.label_bt_MS_VS_Code.Name = "label_bt_MS_VS_Code";
+			this.label_bt_MS_VS_Code.Size = new System.Drawing.Size(94, 16);
+			this.label_bt_MS_VS_Code.TabIndex = 9;
+			this.label_bt_MS_VS_Code.Text = "MS VS Code";
+			// 
+			// label_bt_NotePadPlusPlus
+			// 
+			this.label_bt_NotePadPlusPlus.AutoSize = true;
+			this.label_bt_NotePadPlusPlus.BackColor = System.Drawing.Color.Transparent;
+			this.label_bt_NotePadPlusPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+			this.label_bt_NotePadPlusPlus.ForeColor = System.Drawing.Color.White;
+			this.label_bt_NotePadPlusPlus.Location = new System.Drawing.Point(47, 161);
+			this.label_bt_NotePadPlusPlus.Name = "label_bt_NotePadPlusPlus";
+			this.label_bt_NotePadPlusPlus.Size = new System.Drawing.Size(84, 16);
+			this.label_bt_NotePadPlusPlus.TabIndex = 10;
+			this.label_bt_NotePadPlusPlus.Text = "NotePad++";
+			// 
+			// label_btWOT
+			// 
+			this.label_btWOT.AutoSize = true;
+			this.label_btWOT.BackColor = System.Drawing.Color.Transparent;
+			this.label_btWOT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+			this.label_btWOT.ForeColor = System.Drawing.Color.White;
+			this.label_btWOT.Location = new System.Drawing.Point(32, 203);
+			this.label_btWOT.Name = "label_btWOT";
+			this.label_btWOT.Size = new System.Drawing.Size(114, 16);
+			this.label_btWOT.TabIndex = 11;
+			this.label_btWOT.Text = "World Of Tanks";
+			// 
+			// btnFocus
+			// 
+			this.btnFocus.BackColor = System.Drawing.Color.Transparent;
+			this.btnFocus.FlatAppearance.BorderSize = 0;
+			this.btnFocus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnFocus.Location = new System.Drawing.Point(178, 375);
+			this.btnFocus.Name = "btnFocus";
+			this.btnFocus.Size = new System.Drawing.Size(0, 0);
+			this.btnFocus.TabIndex = 12;
+			this.btnFocus.UseVisualStyleBackColor = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(184, 381);
+			this.Controls.Add(this.btnFocus);
+			this.Controls.Add(this.label_btWOT);
+			this.Controls.Add(this.label_bt_NotePadPlusPlus);
+			this.Controls.Add(this.label_bt_MS_VS_Code);
+			this.Controls.Add(this.label_bt_MS_Visual_Studio);
+			this.Controls.Add(this.label_btAwesomeMiner);
+			this.Controls.Add(this.bt_NotePadPlusPlus);
+			this.Controls.Add(this.bt_MS_Visual_Studio_Code);
+			this.Controls.Add(this.bt_MS_Visual_Studio);
+			this.Controls.Add(this.btWOT);
+			this.Controls.Add(this.btAwesomeMiner);
+			this.Controls.Add(this.btCloseForm);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Location = new System.Drawing.Point(3240, 994);
+			this.Name = "Form1";
+			this.Opacity = 0.9D;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			this.Text = "GadgetQuickLaunch";
+			this.TopMost = true;
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+			}
+
+		#endregion
+		private System.Windows.Forms.Button btCloseForm;
+		private System.Windows.Forms.Button btAwesomeMiner;
+		private System.Windows.Forms.Button btWOT;
+		private System.Windows.Forms.Button bt_MS_Visual_Studio;
+		private System.Windows.Forms.Button bt_MS_Visual_Studio_Code;
+		private System.Windows.Forms.Button bt_NotePadPlusPlus;
+		private System.Windows.Forms.Label label_btAwesomeMiner;
+		private System.Windows.Forms.Label label_bt_MS_Visual_Studio;
+		private System.Windows.Forms.Label label_bt_MS_VS_Code;
+		private System.Windows.Forms.Label label_bt_NotePadPlusPlus;
+		private System.Windows.Forms.Label label_btWOT;
+		private System.Windows.Forms.Button btnFocus;
+		}
+	}
+
---
+++ GadgetQuickLaunch/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace GadgetQuickLaunch
+	{
+
+	public partial class Form1 : Form
+		{
+		public Form1()
+			{
+			
+			Thread.Sleep(3000);				// Делаем задержку запуска приложения, чтобы SideBar не выталкивал её за свои границы и запускался первым.
+			InitializeComponent();
+
+			//-------------------------ЭФФЕКТ ТЕНИ НА НАДПИСИ НА КНОПКЕ ЧЕРЕЗ НАЛОЖЕНИЕ НА НЕЁ LABEL------------------------------------------
+
+			// ДЛЯ КНОПКИ AwesomeMiner
+			// Задаём эффект текста label на названии кнопки, через установлении ПОЛНОЙ прозрачности label поверх кнопки
+			var pos1 = this.PointToScreen(label_btAwesomeMiner.Location);
+			pos1 = btAwesomeMiner.PointToClient(pos1);
+			label_btAwesomeMiner.Parent = btAwesomeMiner;
+			label_btAwesomeMiner.Location = pos1;
+			label_btAwesomeMiner.BackColor = Color.Transparent;
+			// Задаём исчезание и появления эффекта тени при наведении мыши и убирании мыши с кнопки
+			btAwesomeMiner.MouseEnter += (s, e) => label_btAwesomeMiner?.Hide();
+			btAwesomeMiner.MouseLeave += (s, e) => label_btAwesomeMiner?.Show();
+
+			// ДЛЯ КНОПКИ MS VISUAL STUDIO
+			var pos2 = this.PointToScreen(label_bt_MS_Visual_Studio.Location);
+			pos2 = bt_MS_Visual_Studio.PointToClient(pos2);
+			label_bt_MS_Visual_Studio.Parent = bt_MS_Visual_Studio;
+			label_bt_MS_Visual_Studio.Location = pos2;
+			label_bt_MS_Visual_Studio.BackColor = Color.Transparent;
+			bt_MS_Visual_Studio.MouseEnter += (s, e) => label_bt_MS_Visual_Studio.Hide();
+			bt_MS_Visual_Studio.MouseLeave += (s, e) => label_bt_MS_Visual_Studio.Show();
+
+
+			// ДЛЯ КНОПКИ MS VS CODE
+			var pos3 = this.PointToScreen(label_bt_MS_VS_Code.Location);
+			pos3 = bt_MS_Visual_Studio_Code.PointToClient(pos3);
+			label_bt_MS_VS_Code.Parent = bt_MS_Visual_Studio_Code;
+			label_bt_MS_VS_Code.Location = pos3;
+			label_bt_MS_VS_Code.BackColor = Color.Transparent;
+			bt_MS_Visual_Studio_Code.MouseEnter += (s, e) => label_bt_MS_VS_Code.Hide();
+			bt_MS_Visual_Studio_Code.MouseLeave += (s, e) => label_bt_MS_VS_Code.Show();
+
+			// ДЛЯ КНОПКИ NOTEPAD++
+			var pos4 = this.PointToScreen(label_bt_NotePadPlusPlus.Location);
+			pos4 = bt_NotePadPlusPlus.PointToClient(pos4);
+			label_bt_NotePadPlusPlus.Parent = bt_NotePadPlusPlus;
+			label_bt_NotePadPlusPlus.Location = pos4;
+			label_bt_NotePadPlusPlus.BackColor = Color.Transparent;
+			bt_NotePadPlusPlus.MouseEnter += (s, e) => label_bt_NotePadPlusPlus.Hide();
+			bt_NotePadPlusPlus.MouseLeave += (s, e) => label_bt_NotePadPlusPlus.Show();
+
+			// ДЛЯ КНОПКИ WORLD OF TANKS
+			var pos5 = this.PointToScreen(label_btWOT.Location);
+			pos5 = btWOT.PointToClient(pos5);
+			label_btWOT.Parent = btWOT;
+			label_btWOT.Location = pos5;
+			label_btWOT.BackColor = Color.Transparent;
+			btWOT.MouseEnter += (s, e) => label_btWOT.Hide();
+			btWOT.MouseLeave += (s, e) => label_btWOT.Show();
+//------------------------------------------------------------------------------------------------------------------------------------------
+			}
+		
+
+		// Задаём расположение запусак формы, в данном случае нижний правый угол.  Обязательно ставим StartPosition: Manual в свойствах формы!
+		private void Form1_Load(object sender, EventArgs e)
+			{
+			Location = new Point(3240, 994);
+			}
+
+		// Закрытие формы
+		public void btCloseForm_Click(object sender, EventArgs e)
+			{
+			Close();
+			}
+
+		// Запуск майнинг программы Awesome Miner
+		private void btAwesomeMiner_Click(object sender, EventArgs e)
+			{
+			Process.Start(@"D:\INVESTING\Awesome Miner\AwesomeMiner.exe");
+
+			// ссылка на кнопку btnFocus с указание фокуса на неё необходима для удаления остаточного эффекта чёрной рамки
+			// параметры кнопки btnFocus: размеры 0х0 пикселей, visible = true, enabled = true; color = transparent (internet) - все параметры необходимы для невидимости этой кнопки
+			// расположение кнопки btnFocus в нижнем правом углу формы
+			btnFocus.Focus();
+			}
+		
+		private void bt_MS_Visual_Studio_Click(object sender, EventArgs e)
+			{
+			Process.Start(@"H:\Microsoft Visual Studio\Studio\Common7\IDE\devenv.exe");
+			btnFocus.Focus();
+			}
+
+		private void bt_MS_Visual_Studio_Code_Click(object sender, EventArgs e)
+			{
+			Process.Start(@"H:\Microsoft_VS_Code\Code.exe --disable - gpu");
+			btnFocus.Focus();
+			}
+
+		private void bt_NotePadPlusPlus_Click(object sender, EventArgs e)
+			{
+			Process.Start(@"C:\Program Files\Notepad++\notepad++.exe");
+			btnFocus.Focus();
+			}
+
+		private void btWOT_Click(object sender, EventArgs e)
+			{
+			Process.Start(@"H:\Wargaming.net\GameCenter\wgc.exe");
+			btnFocus.Focus();
+			}
+
+		}
+
+	}
+
